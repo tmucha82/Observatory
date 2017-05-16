0006bfa0e40edfe5497c6e5521858531ce1a7b87
tmucha82 <tmucha82@gmail.com>
2017-05-16 15:33:02 +0200

    week3 - first blood

--- src/test/scala/observatory/InteractionTest.scala
+++ src/test/scala/observatory/InteractionTest.scala
@@ -5,9 +5,18 @@ import org.scalatest.FunSuite
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.prop.Checkers
 
-import scala.collection.concurrent.TrieMap
-
 @RunWith(classOf[JUnitRunner])
 class InteractionTest extends FunSuite with Checkers {
+
+  trait TestSet {
+
+  }
+
+  test("") {
+    new TestSet {
+
+    }
+    ()
+  }
 
 }
